4251f5f8abb17ed9e5d9e87d20318994f2aac3e5
Weihu Chen <cctiger36@gmail.com>
2013-10-25 12:41:48 +0900

    refactoring

--- lib/shadow_model/associations/has_many.rb
+++ lib/shadow_model/associations/has_many.rb
@@ -12,43 +12,52 @@ class ::ActiveRecord::Associations::Builder::HasMany
     reflection = build_without_shadow_option
     if options[:shadow]
       model_name = model.name.tr('::', '').underscore
-      model.class_eval <<-RUBY
-        def #{name}_shadow_cache_key
-          "#{model_name}_\#{self[self.class.primary_key]}_#{name}_shadow_cache"
-        end
-
-        def #{name}_by_shadow
-          Redis.current.hvals(#{name}_shadow_cache_key).map do |val|
-            shadow_data = Marshal.load(val)
-            #{reflection.klass.name}.instantiate_shadow_model(shadow_data)
-          end
-        end
-
-        def clear_#{name}_shadow_cache
-          Redis.current.del(#{name}_shadow_cache_key)
-        end
-      RUBY
-
-      reflection.klass.class_eval <<-RUBY
-        after_save :update_shadow_cache_of_#{model_name}
-        after_destroy :delete_shadow_cache_of_#{model_name}
-
-        def belongs_to_#{model_name}_shadow_cache_key
-          "#{model_name}_\#{self[:#{reflection.foreign_key}]}_#{name}_shadow_cache"
-        end
-
-        def update_shadow_cache_of_#{model_name}
-          cache_key = belongs_to_#{model_name}_shadow_cache_key
-          Redis.current.hset(cache_key, self[self.class.primary_key], self.build_shadow_data)
-          update_expiration(cache_key)
-        end
-
-        def delete_shadow_cache_of_#{model_name}
-          Redis.current.hdel(belongs_to_#{model_name}_shadow_cache_key, self[self.class.primary_key])
-        end
-      RUBY
+      define_shadow_methods_of_model_class(reflection, model_name)
+      define_shadow_methods_of_reflection_class(reflection, model_name)
     end
     reflection
   end
   alias_method_chain :build, :shadow_option
+
+  private
+
+  def define_shadow_methods_of_model_class(reflection, model_name)
+    model.class_eval <<-RUBY
+      def #{name}_shadow_cache_key
+        "#{model_name}_\#{self[self.class.primary_key]}_#{name}_shadow_cache"
+      end
+
+      def #{name}_by_shadow
+        Redis.current.hvals(#{name}_shadow_cache_key).map do |val|
+          shadow_data = Marshal.load(val)
+          #{reflection.klass.name}.instantiate_shadow_model(shadow_data)
+        end
+      end
+
+      def clear_#{name}_shadow_cache
+        Redis.current.del(#{name}_shadow_cache_key)
+      end
+    RUBY
+  end
+
+  def define_shadow_methods_of_reflection_class(reflection, model_name)
+    reflection.klass.class_eval <<-RUBY
+      after_save :update_shadow_cache_of_#{model_name}
+      after_destroy :delete_shadow_cache_of_#{model_name}
+
+      def belongs_to_#{model_name}_shadow_cache_key
+        "#{model_name}_\#{self[:#{reflection.foreign_key}]}_#{name}_shadow_cache"
+      end
+
+      def update_shadow_cache_of_#{model_name}
+        cache_key = belongs_to_#{model_name}_shadow_cache_key
+        Redis.current.hset(cache_key, self[self.class.primary_key], self.build_shadow_data)
+        update_expiration(cache_key)
+      end
+
+      def delete_shadow_cache_of_#{model_name}
+        Redis.current.hdel(belongs_to_#{model_name}_shadow_cache_key, self[self.class.primary_key])
+      end
+    RUBY
+  end
 end
